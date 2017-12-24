51ccc7f6a53d12bd11a7b3e047f5cbc25deb02a3
Itay Fridman <fitay123@gmail.com>
2017-12-24 17:13:56 +0200

    change database

--- core/init.php
+++ core/init.php
@@ -1,16 +1,16 @@
 <?php
 // MySQL connection variables
-define('ROOT_URL','http://localhost/cytrivia1/cytrivia/');
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASSWORD','12345');
-define('DB_NAME','cytrivia');
+// define('ROOT_URL','http://localhost/cytrivia1/cytrivia/');
+// define('DB_HOST','localhost');
+// define('DB_USER','root');
+// define('DB_PASSWORD','12345');
+// define('DB_NAME','cytrivia');
 
-// define('ROOT_URL','http://cytrivia.ml/');
-// define('DB_HOST','sql113.epizy.com');
-// define('DB_USER','epiz_21213311');
-// define('DB_PASSWORD','JwlIM5G8uNOZ');
-// define('DB_NAME','epiz_21213311_cytrivia');
+define('ROOT_URL','http://cytrivia.ml/');
+define('DB_HOST','sql113.epizy.com');
+define('DB_USER','epiz_21213311');
+define('DB_PASSWORD','JwlIM5G8uNOZ');
+define('DB_NAME','epiz_21213311_cytrivia');
 // Connect to MySQL using MySQLi
 $con = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 session_start();
