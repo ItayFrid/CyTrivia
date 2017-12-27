2eabdf644b4b8353eb725d097590dc077e5293e3
salidalal <34741501+salidalal@users.noreply.github.com>
2017-12-27 13:24:25 +0200

    Update befortrivia.php

--- befortrivia.php
+++ befortrivia.php
@@ -2,20 +2,30 @@
 
 
 <h1 style="text-align: center;" dir="RTL">&nbsp;</h1>
-<h1 style="text-align: center;" dir="RTL"><span style="font-family: Segoe UI; font-size: xxx-large;">גאוני סייבר היקרים,</span></h1>
+<h1 style="text-align: center;" dir="RTL">&nbsp;</h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-family: helvetica; font-size: xx-large;">גאוני סייבר היקרים,</span></h1>
 <p>&nbsp;</p>
-<h1 style="text-align: center;" dir="RTL"><span style="font-family: Segoe UI; font-size: x-large;">ברוכים הבאים לחידון הסייבר הגדול של אומת הסטרטאפ הישראלי!</span></h1>
-<h1 style="text-align: center;" dir="RTL"><span style="font-family: Segoe UI; font-size: x-large;">כחלק מיוזמה לקידום ועידוד חינוך טכנולוגי החלטנו לערוך תחרות בניכם , גאוני הסייבר.</span></h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-family: helvetica; font-size: x-large;">ברוכים הבאים לחידון הסייבר הגדול של אומת הסטרטאפ הישראלי!</span></h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-family: helvetica; font-size: x-large;">כחלק מיוזמה לקידום ועידוד חינוך טכנולוגי החלטנו לערוך תחרות בניכם , גאוני הסייבר.</span></h1>
 <p>&nbsp;</p>
-<h1 style="text-align: center;" dir="RTL"><span style="font-size: large;"><span style="font-family: Segoe UI;">המטרה שלכם היא כמובן לקבל תוצאה מושלמת&nbsp;</span>ואל תחשבו שזה הולך להיות קל ופשוט.</span></h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-size: large;"><span style="font-family: helvetica;">המטרה שלכם היא כמובן לקבל תוצאה מושלמת&nbsp;</span>ואל תחשבו שזה הולך להיות קל ופשוט.</span></h1>
 <h1 style="text-align: center;" dir="RTL"><span style="font-size: large;">לפניכם 10 שאלות ידע בנושא סייבר שיבדקו מה אתם באמת יודעים!</span></h1>
-<h1 style="text-align: center;" dir="RTL"><span style="font-family: Segoe UI; font-size: large;">המטרה שלנו היא לבחור את הטובים מבניכם שיצטרפו אלינו כחברים באומה ויזכו במלגה שווה במיוחד!</span></h1>
-<h1 style="text-align: center;" dir="RTL"><span style="font-family: Segoe UI; font-size: large;">&nbsp;</span></h1>
-<h1 style="text-align: center;" dir="RTL"><span style="font-family: Segoe UI; font-size: large;">&nbsp;</span></h1>
-<h1 style="text-align: center;" dir="RTL"><span style="font-size: large;"><span style="font-family: Segoe UI;">במהלך המשחק יוצגו לכם 10 שאלות&nbsp;</span>ברמת קושי שתתחיל מהרמה הבסיסית ביותר</span></h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-family: helvetica; font-size: large;">המטרה שלנו היא לבחור את הטובים מבניכם שיצטרפו אלינו כחברים באומה ויזכו במלגה שווה במיוחד!</span></h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-family: helvetica; font-size: large;">&nbsp;</span></h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-family: helvetica; font-size: large;">&nbsp;</span></h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-size: large;"><span style="font-family: helvetica;">במהלך המשחק יוצגו לכם 10 שאלות&nbsp;</span>ברמת קושי שתתחיל מהרמה הבסיסית ביותר</span></h1>
 <h1 style="text-align: center;" dir="RTL"><span style="font-size: large;">ושתעלה לאחר מענה נכון ברמה הנוכחית או לאחר מענה תשובות לא נכונות על כל השאלות מהרמה הנוכחית.</span></h1>
-<h1 style="text-align: center;" dir="RTL"><span style="font-family: Segoe UI; font-size: large;">קיימות 10 רמות קושי שונות , ניקוד של תשובה נכונה עולה בהתאם לרמה ויחס לזמן המענה.</span></h1>
-<h1 style="text-align: center;" dir="RTL"><span style="font-family: Segoe UI; font-size: large;">כל חבר יכול להשתתף בתחרות פעם אחת בלבד !</span></h1>
-<h1 style="text-align: center;" dir="RTL"><span style="font-family: Segoe UI; font-size: x-large;">בהצלחה!</span></h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-family: helvetica; font-size: large;">קיימות 10 רמות קושי שונות , ניקוד של תשובה נכונה עולה בהתאם לרמה ויחס לזמן המענה.</span></h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-family: helvetica; font-size: large;">כל חבר יכול להשתתף בתחרות פעם אחת בלבד !</span></h1>
+<h1 style="text-align: center;" dir="RTL"><span style="font-family: helvetica; font-size: x-large;">בהצלחה!</span></h1>
 
-<?php include('includes/footer.php');?>
+<?php
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+		aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+<a href="<?php echo ROOT_URL;?>play.php">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">התחל!</button>
+            </a>
+
+ include('includes/footer.php');?>
