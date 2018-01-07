a6646d6a57b0351125bc033a207445161d94222e
Salidalal_ <salidalala@gmail.com>
2018-01-07 11:17:40 +0200

    unit tes2

--- TestRandArray.php
+++ TestRandArray.php
@@ -6,7 +6,7 @@ function Test_if_TestrandomizeAnswers_returnrs_4_ans( $arr= (array("test1", "tes
   $testarr = randomizeAnswers ($arr);
   echo "check if the function return the same nummber of the elment its get\n";
   echo count(&testarr) == count($arr);
-  print"\n"
+  print"\n";
 
 }
 
@@ -17,13 +17,13 @@ function Test_if_TestrandomizeAnswers_contain_all_the_values_he_get($arr= (array
     echo in_array($val);
   }
 
-  print"\n"
+  print"\n";
 }
 
 function Test_if_generateQuestions_returns_10_levels( $testarr = generateQuestions() ){
   echo "check if the function returns an array with 10 elements \n";
-  echo (in_array ($testarr,"test1") and in_array ($testarr,"test2") and in_array ($testarr,"test3") and in_array ($testarr,"test4"))
-  print"\n"
+  echo count $testarr == 10;
+  print"\n";
 }
 
 
